9e6ec6478318219971451fec465dd780d1f677f3
edsonjfs <edsonjosefs@gmail.com>
2021-10-04 17:19:26 -0300

    calculadora

---
+++ Documentos/OdinTest/Java/Devdojo/maratonaJava/src/academy/devdojo/maratonajava/introducao/Aula04OperadoresUnarios.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04OperadoresUnarios {
+    public static void main(String[] args) {
+        double n1=10;
+        double n2= 20;
+        double res= n1/n2;
+        System.out.println(res);
+
+
+        
+    }
+
+    
+}
---
+++ Documentos/OdinTest/Java/Devdojo/maratonaJava/src/academy/devdojo/maratonajava/introducao/Ex001.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Ex001 {
+    public static void main(String[] args) {
+
+        String nome="Edson";
+        String endereco= "Rua Olivo Pasqualini, 122";
+        String data="04/08/2021";
+        double  salario=3000.00;
+
+        System.out.println("Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salario de R$"+salario+", na data "+data);
+
+
+        
+    }
+}
